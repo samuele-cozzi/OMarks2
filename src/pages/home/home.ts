9595874a06b08a65c4fa2eebfcba0c0fcd599da8
samuele <samuele.cozzi@gmail.com>
2017-09-02 23:21:25 +0200

    .

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, HostListener } from '@angular/core';
 import { NavController, Searchbar, ToastController, NavParams } from 'ionic-angular';
- 
-import {SettingsModel} from '../../models/settingsModel';
-import {Marks} from '../../models/marks';
-import {EditItemPage} from '../edit_item/edit_item';
-import {Settings} from '../../providers/settings';
-import {AlgoliaService} from '../../providers/algolia';
+
+import { SettingsModel } from '../../models/settingsModel';
+import { Marks } from '../../models/marks';
+import { EditItemPage } from '../edit_item/edit_item';
+import { Settings } from '../../providers/settings';
+import { AlgoliaService } from '../../providers/algolia';
 
 
 @Component({
@@ -14,7 +14,22 @@ import {AlgoliaService} from '../../providers/algolia';
 })
 export class HomePage {
   @ViewChild('searchBar') searchBar: Searchbar;
-  
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+    if (event.key == 'Backspace' && event.isTrusted && event.ctrlKey) {
+      this.toDashboard();
+    }
+    if (event.key == 'Enter' && event.isTrusted && event.ctrlKey) {
+      this.toogleSearchBar();
+    }
+    if (event.key == 'Enter' && event.isTrusted && !event.ctrlKey) {
+      if(this.searchItems.length > 0){
+        this.open(this.searchItems[0], null);
+      }
+    }
+  }
+
   private showSearch: boolean = false;
   private page: number = 0;
   private query: string = "";
@@ -25,7 +40,7 @@ export class HomePage {
   dashboardItems: any[] = [];
   searchFacets: any[] = [];
   searchItems: any[] = [];
-  
+
   /* IONIC Lifecycle app */
   /**
    * 
@@ -33,22 +48,22 @@ export class HomePage {
    * @param settings 
    * @param algolia 
    */
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     private navParams: NavParams,
-    private toastCtrl: ToastController, 
-    settings: Settings, 
+    private toastCtrl: ToastController,
+    settings: Settings,
     private algoliaService: AlgoliaService) {
-    
-      this.user = settings.getSettings();
-      this.key = navParams.get('key');
-      this.value = navParams.get('value');
+
+    this.user = settings.getSettings();
+    this.key = navParams.get('key');
+    this.value = navParams.get('value');
   }
 
   /**
    * 
    */
   ionViewDidLoad() {
-    if (this.key !== undefined && this.value !== undefined){
+    if (this.key !== undefined && this.value !== undefined) {
       this.getSearchFacets(this.key, this.value);
       this.showSearch = true;
     } else {
@@ -57,19 +72,19 @@ export class HomePage {
   }
 
   /* HEADER_EVENTS */
-  onInputSearch(event){
+  onInputSearch(event) {
     console.log(event);
     (event.inputType === "insertText") && (event.target.value !== "") && this.getSearch(event.target.value, 0);
     (event.inputType === "deleteContentBackward") && (event.target.value === "") && (this.searchItems = []);
   }
 
-  onCancelSearch(event){
+  onCancelSearch(event) {
     console.log(event);
     this.showSearch = false;
     this.searchItems = [];
   }
 
-  onClearSearch(event){
+  onClearSearch(event) {
     console.log(event);
     this.searchItems = [];
   }
@@ -80,6 +95,11 @@ export class HomePage {
     if (this.showSearch) {
       setTimeout(() => { this.searchBar.setFocus(); });
     }
+  }
+  
+  toDashboard() {
+    this.getFacets();
+    this.showSearch = false;
   }
 
   /* Toast functions */
@@ -105,34 +125,34 @@ export class HomePage {
   }
 
 
-  getDashboard(){
+  getDashboard() {
     this.algoliaService.get_dashboard().then(items => { this.dashboardItems = items });
   }
 
-  getFacets(){
+  getFacets() {
     this.algoliaService.get_facets().then(items => {
       this.searchItems = [];
       this.searchFacets = items;
     });
   }
 
-  async getSearch(query, page){
+  async getSearch(query, page) {
     (this.query != query) && (this.initParams());
     this.query = query;
-    var items = await this.algoliaService.get_query(query, 20, page).then(); 
+    var items = await this.algoliaService.get_query(query, 20, page).then();
     for (var i = 0; i < items.length; i++) {
-      this.searchItems.push( items[i] );
+      this.searchItems.push(items[i]);
     }
     return items.length;
   }
 
-  async getSearchFacets(key, value){
+  async getSearchFacets(key, value) {
     (this.key != key || this.value != value) && (this.initParams());
     this.key = key;
     this.value = value;
-    var items = await this.algoliaService.get_filtered_facets(key, value, 20, this.page).then(); 
+    var items = await this.algoliaService.get_filtered_facets(key, value, 20, this.page).then();
     for (var i = 0; i < items.length; i++) {
-      this.searchItems.push( items[i] );
+      this.searchItems.push(items[i]);
     }
     return items.length;
   }
@@ -143,82 +163,79 @@ export class HomePage {
   }
 
   async doInfinite(infiniteScroll) {
-    if (this.searchItems.length > 0)
-    {
-        this.page ++;
-        var newItemsLength = 0;
-        console.log('Begin async operation: ' + this.page);
-        if (this.query.trim() != '') {
-          newItemsLength = await this.getSearch(this.query,this.page);
-          (newItemsLength == 0) && infiniteScroll.enable(false);
-          infiniteScroll.complete();
-        } 
-        else if (this.key != "" && this.value != "")
-        {
-          newItemsLength = await this.getSearchFacets(this.key, this.value);
-          (newItemsLength == 0) && infiniteScroll.enable(false);
-          infiniteScroll.complete();
-        }
-    }
-    else
-    {
+    if (this.searchItems.length > 0) {
+      this.page++;
+      var newItemsLength = 0;
+      console.log('Begin async operation: ' + this.page);
+      if (this.query.trim() != '') {
+        newItemsLength = await this.getSearch(this.query, this.page);
+        (newItemsLength == 0) && infiniteScroll.enable(false);
         infiniteScroll.complete();
+      }
+      else if (this.key != "" && this.value != "") {
+        newItemsLength = await this.getSearchFacets(this.key, this.value);
+        (newItemsLength == 0) && infiniteScroll.enable(false);
+        infiniteScroll.complete();
+      }
+    }
+    else {
+      infiniteScroll.complete();
     }
   }
 
   /* DASHBOARD EVENTS */
-  open(item,event){
+  open(item, event) {
     typeof item.time_read == "string" && (item.time_read = 0);
-    
+
     this.algoliaService.save_item(item);
     window.open(item.given_url)
   }
 
-  create(item){
-      this.navCtrl.push(EditItemPage, {
-          item:  JSON.stringify(new Marks(), null, 2)
-      });
+  create(item) {
+    this.navCtrl.push(EditItemPage, {
+      item: JSON.stringify(new Marks(), null, 2)
+    });
   }
 
-  edit(item){
+  edit(item) {
     this.navCtrl.push(EditItemPage, {
       item: JSON.stringify(item, null, 2)
     });
   }
 
-  up(item){
-    item.time_read ++;
+  up(item) {
+    item.time_read++;
     this.algoliaService.save_item(item)
-        .then(x => this.toastSavedDashboard())
-        .catch(err => this.toastError(err));
+      .then(x => this.toastSavedDashboard())
+      .catch(err => this.toastError(err));
   }
 
-  down(item){
-    item.time_read --;
+  down(item) {
+    item.time_read--;
     this.algoliaService.save_item(item)
-        .then(x => this.toastSavedDashboard())
-        .catch(err => this.toastError(err));
+      .then(x => this.toastSavedDashboard())
+      .catch(err => this.toastError(err));
   }
 
-  add_star(item){
+  add_star(item) {
     item.favorite = 1;
     this.algoliaService.save_item(item)
       .then(x => this.toastSavedDashboard())
       .catch(err => this.toastError(err));
   }
 
-  remove_star(item){
+  remove_star(item) {
     item.favorite = 0;
     this.algoliaService.save_item(item)
       .then(x => this.toastSavedDashboard())
       .catch(err => this.toastError(err));
   }
 
-  delete(item){
+  delete(item) {
     this.algoliaService.delete_item(item)
       .then(x => this.toastSavedDashboard())
       .catch(err => this.toastError(err));
-}
+  }
 
   /* SEARCH EVENTS */
 
